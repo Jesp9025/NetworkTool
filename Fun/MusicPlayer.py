592a3698bc257760b112da64a46fdfde98111148
Jesper Pedersen <54931767+Jesp9025@users.noreply.github.com>
2019-11-19 22:44:34 +0100

    Update MusicPlayer.py

--- Fun/MusicPlayer.py
+++ Fun/MusicPlayer.py
@@ -3,23 +3,23 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide" #This hides the pygame message
 import pygame #Imports pygame to play sounds
 
 def setSong(): #Initializes the music player, loads a song, sets volume and plays it
-    pygame.mixer.init()
+    pygame.mixer.init(44100)
     pygame.mixer.music.load('sounds/sound.mp3')
     pygame.mixer.music.set_volume(0.2)
     pygame.mixer.music.play()
 
 def setSong2():
-    pygame.mixer.init()
+    pygame.mixer.init(44100)
     pygame.mixer.music.set_volume(0.2)    
     pygame.mixer.music.load('sounds/sound2.mp3')
     pygame.mixer.music.play()
 
 def setSong3():
-    pygame.mixer.init()
+    pygame.mixer.init(44100)
     pygame.mixer.music.set_volume(0.2)    
     pygame.mixer.music.load('sounds/sound3.mp3')
     pygame.mixer.music.play()
 
 def stopMusic(): #stops the music
-    pygame.mixer.init()
+    pygame.mixer.init(44100)
     pygame.mixer.music.stop()
