218558fd5fbfb75864bdca76ea6250f1c8cd3911
Jesper Pedersen <54931767+Jesp9025@users.noreply.github.com>
2019-11-29 11:34:03 +0100

    Update WhoIs.py

--- Fun/WhoIs.py
+++ Fun/WhoIs.py
@@ -1,4 +1,6 @@
 #https://github.com/richardpenman/pywhois
+#https://www.pythonforbeginners.com/dns/using-pywhois
 import whois
 w = whois.whois("google.com")
 print(w)
+#It may say there is an error and whois is not callable.. Ignore it.
