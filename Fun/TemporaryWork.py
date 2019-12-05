ea1634defb94fbf28080cc7253e8717b22aed248
Jesper Pedersen <54931767+Jesp9025@users.noreply.github.com>
2019-12-05 15:58:48 +0100

    Fixed port scanner and added inputs.  Made more comments on stuff

--- Fun/TemporaryWork.py
+++ Fun/TemporaryWork.py
@@ -9,20 +9,19 @@ import time # To start a timer for port scanner
 from decimal import Decimal #To round numbers
 import itertools #For loading animation
 
-#To start and stop loading animation. True = Not loading, False = Loading
-done = True
 
-
-#######################
-#Functions
-#######################
+#####################################################################
+#                           Functions
+#####################################################################
 #Clears the window
 def clearWindow():
     window["_INFO_"].update("")
     
-#Func to run commands and update text
+
+
+############# Run CMD ##############
 #Uses subprocess, which includes stdout, stderr. Using communicate() allows you to get output and error from the subprocess.Popen
-def runcmd(cmd, ipreq):
+def runcmd(cmd, ipreq): # cmd argument is used to determine which command to run. ipreq is used to determine if input from _IP_ is needed or can be ignored. ipreq has to be 1 or 0
     global done
     clearWindow()
     if ipreq == 1:
@@ -48,7 +47,6 @@ def runcmd(cmd, ipreq):
             clearWindow()
             print("Unknown command")
             
-        
 
 #Function to show ipconfig for Windows and Linux
 def ipconfigFunc():
@@ -95,15 +93,17 @@ def shutdownPC():
 #https://stackoverflow.com/a/44666336
 #This will create a new thread that runs a timer, and then runs the function, without freezing the program
 def fun():
-    MusicPlayer.setSong3()
+    MusicPlayer.setCountdown()
     threading.Timer(17, shutdownPC).start()
 
+############# Whois ##############
 #https://github.com/richardpenman/pywhois
 #https://www.pythonforbeginners.com/dns/using-pywhois
 #Uses module 'python-whois'
 def runWhois():
     global done
     
+    # If input is not empty, do stuff
     if values["_whoisInput_"] != "":
         clearWindow()
         done = False
@@ -116,7 +116,7 @@ def runWhois():
         print("Please enter a target")
 #It may or may not say there is an error and whois is not callable.. Ignore it.
 
-############# Port Scanner #############
+############# Port Scanner ##############
 #https://pythonprogramming.net/python-threaded-port-scanner/?completed=/python-port-scanner-sockets/
 #Added s.settimeout() to make it faster, instead of using default timeout
 
@@ -125,13 +125,13 @@ def portscan(port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(0.5)
     try:
-        
         con = s.connect((values["_PORTINPUT_"], port))
         with print_lock:
             print('port', port)
         con.close()
     except:
         pass
+
 _FINISH = False
 # The threader thread pulls a worker from the queue and processes it
 def threader():
@@ -148,49 +148,71 @@ def threader():
         # completed with the job
         q.task_done()
 
+
 def startPortScan():
     clearWindow()
     global q
     global print_lock
     global _FINISH
+
+    # print_lock makes sure that once a thread has started "working" on a task/variable, other threads can not use it
     print_lock = threading.Lock()
+
     # Create the queue
     q = Queue()
 
     # how many threads are we going to allow for
+    # Checks if target input is empty
     if values["_PORTINPUT_"] != "":
-        for x in range(800):
-            z = threading.Thread(target=threader, daemon=True)
-            z.start()
+        try:
+            # Checks if inputs are lower than or equal to 0
+            if int(values["_STARTPORT_"]) <= 0 or int(values["_ENDPORT_"]) <= 0:
+                print("Start and end port must be higher than 0")
+            else:
+                # Checks if endport is higher than startport
+                if int(values["_ENDPORT_"]) >= int(values["_STARTPORT_"]) and int(values["_ENDPORT_"]) <= 65535:
 
-        #Start time
-        start = time.time()
+                    # Used to determine how many threads to create
+                    temp = int(values["_ENDPORT_"]) - int(values["_STARTPORT_"]) + 1
 
-        # Number of ports to scan
-        #Problem right now is that if you choose more ports than there are threads, it will hang.
-        if values["_SMALLSCAN_"]:
-            for worker in range(1, 2000):
-                q.put(worker)
-        elif values["_BIGSCAN_"]:
-            for worker in range(1, 65535):
-                q.put(worker)
+                    # If temp is lower than 800, it should only create the needed amount of threads
+                    if temp < 800:
+                        for x in range(temp):
+                            z = threading.Thread(target=threader, daemon=True)
+                            z.start()
+                    else:
+                        # 800 is almost the max amount of threads possible to run at same time
+                        for x in range(800):
+                            z = threading.Thread(target=threader, daemon=True)
+                            z.start()
 
-        _FINISH = True
-        # wait until the thread terminates.
-        q.join()
+                    #Start time
+                    start = time.time()
 
-        #End time
-        end = time.time()
-        if values["_SMALLSCAN_"]:
-            print("Scan completed. Time elapsed:", round(end - start, 2), "seconds\nPorts scanned: 2000")
-        elif values["_BIGSCAN_"]:
-            print("Scan completed. Time elapsed:", round(end - start, 2), "seconds\nPorts scanned: 65535")
-        # Remember to set _FINISH to False
-        _FINISH = False
+                    # Number of ports to scan
+                    for worker in range(int(values["_STARTPORT_"]), int(values["_ENDPORT_"]) +1):
+                        q.put(worker)
+
+                    _FINISH = True
+                    # wait until the thread terminates.
+                    q.join()
+
+                    #End time
+                    end = time.time()
+
+                    print("Scan completed in", round(end - start, 2), "seconds\nNumber of ports scanned:", temp)
+
+                    # Remember to set _FINISH to False
+                    _FINISH = False
+                else:
+                    print("Can't go higher than 65535,\nand end port must be higher than start port")
+        # Returns a printed error if non-int has been input
+        except ValueError:
+            print("Incorrect input. You either didn't type port numbers, or you wrote a non-Int")
     else:
             print("Please enter a target")
 
-#Loading animation
+############# Loading Animation ##############
 #Very basic. We might find something better
 #https://stackoverflow.com/a/22029635
 def animate():
@@ -199,17 +221,20 @@ def animate():
             break
         window["_INFO_"].update('\rloading ' + c)
         time.sleep(0.1)
-        
+
+#To start and stop loading animation. True = Not loading, False = Loading
+done = True
 
 
-#######################
-#GUI Code
-#######################
+#####################################################################
+#                           GUI Code
+#####################################################################
+
 #Sets the theme
 sg.change_look_and_feel('Reddit')
 
 #Creates the layouts for each tab
-#MusicPlayer Frame. Uses PyGame to play sounds, control volume etc.
+#MusicPlayer tab. Uses PyGame to play sounds, control volume etc.
 tab1_layout =  [[sg.Text("")],
                 [sg.Text("Some music to enjoy while troubleshooting")], 
                 [sg.Button(button_text="Play Music"),
@@ -222,6 +247,7 @@ tab1_layout =  [[sg.Text("")],
                 "Eye Of The Tiger"],
                 default_value="Push It To The Limit", key="_SONG_")]]
 
+#Basic tools tab
 tab2_layout = [[sg.Frame(layout=[
     [sg.Radio('Windows', "RADIO1", default=True, key="_WINDOWS_"), #key will make it possible to do events and use value of the button, depending on if its pressed or not(True, False)
         sg.Radio('Linux', "RADIO1", key="_LINUX_")]],
@@ -237,36 +263,43 @@ tab2_layout = [[sg.Frame(layout=[
     [sg.Text("")],
     [sg.Button("Custom Command")]]
 
+#Whois tab
 tab3_layout = [[sg.Text("Here you can run a whois on a URL")],
                 [sg.T('Target:'), sg.InputText(key="_whoisInput_")],
                 [sg.Button("Run Whois")]]
 
+#Port scanner tab
 tab4_layout = [[sg.Text("Here you can scan a range of ports")],
     [sg.Text("Target:"), sg.Input(key="_PORTINPUT_")],
     [sg.Button("Run PortScan"), sg.Text("Hackthissite.org is legal to scan")],
     [sg.Frame(layout=[
-    [sg.Radio('Small scan', "RADIO2", default=True, key="_SMALLSCAN_"), #key will make it possible to do events and use value of the button, depending on if its pressed or not(True, False)
-        sg.Radio('Big scan', "RADIO2", key="_BIGSCAN_")]],
+    [sg.Text("Start Port:"), sg.Input(key="_STARTPORT_", size=(10,1))],
+    [sg.Text("End Port: "), sg.Input(key="_ENDPORT_", size=(10,1))]],
             title='Scan Size',
             title_color='black',
             relief=sg.RELIEF_SUNKEN)],
-    [sg.Text("Small scan scans 1-2000, takes a few seconds\nBig scan scans 1-65535, takes 40-70 seconds")]]
+    [sg.Text("Scanning 65535 ports takes 40-60 seconds.")]]
 
-tab5_layout = [[sg.T('This is inside tab 5 / Wi-Fi Explorer')]]
+#Wifi-Analyzer tab
+tab5_layout = [[sg.T('This is inside tab 5 / Wi-Fi Analyzer')]]
 
+#IP Calculator tab
 tab6_layout = [[sg.T('This is inside tab 6 / IP (VLSM) Calculator')]]
 
+#Cisco IOS tab
 tab7_layout = [[sg.T('This is inside tab 7 / Cisco IOS')]]
 
 tab8_layout = [[sg.T('This is inside tab 8')]]
 
 #This is where all layots/tabs are put together, to be used in window
 layout = [[sg.TabGroup([[sg.Tab('Music Player', tab1_layout), sg.Tab('Basics', tab2_layout),
-            sg.Tab('Whois', tab3_layout), sg.Tab('Port Scanner', tab4_layout), sg.Tab('Wi-Fi Explorer', tab5_layout),
+            sg.Tab('Whois', tab3_layout), sg.Tab('Port Scanner', tab4_layout), sg.Tab('Wi-Fi Analyzer', tab5_layout),
             sg.Tab('IP (VLSM) Calculator', tab6_layout),sg.Tab('Cisco IOS', tab7_layout), sg.Tab('Tab 8', tab8_layout)]]),
             sg.Output(size=(46, 18), key="_INFO_")]]
+
 #Creates the window
 window = sg.Window('Network Tool', layout, default_element_size=(12,1))    
+
 
 #Runs the window in a loop
 while True:    
