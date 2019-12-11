aca81b295914c8c4fdbdc321ed5770b7db1871f6
Jesper Pedersen <54931767+Jesp9025@users.noreply.github.com>
2019-12-11 10:14:11 +0100

    Update MusicPlayer.py

--- NetworkTool/MusicPlayer.py
+++ NetworkTool/MusicPlayer.py
@@ -1,6 +1,8 @@
-import os
+import os, sys
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide" #This hides the pygame message. Annoying..
 import pygame #Imports pygame to play sounds
+
+APP_FOLDER = os.path.dirname(os.path.realpath(sys.argv[0]))
 
 #Initialize the mixer and set volume
 pygame.mixer.init(44100)
@@ -25,20 +27,23 @@ def volumeDOWN():
         pygame.mixer.music.set_volume(volume)
 
 def setSong1(): #Loads a song and plays it
-    pygame.mixer.music.load('sounds/sound1.mp3')
-    #(-1) means that song is repeated
-    pygame.mixer.music.play(-1)
+    full_path = os.path.join(APP_FOLDER, "sound1.mp3")
+    mixer.music.load(full_path)
+    pygame.mixer.music.play(-1) #(-1) means that song is repeated
 
 def setSong2():
-    pygame.mixer.music.load('sounds/sound2.mp3')
+    full_path = os.path.join(APP_FOLDER, "sound2.mp3")
+    mixer.music.load(full_path)
     pygame.mixer.music.play(-1)
 
 def setSong3():
-    pygame.mixer.music.load('sounds/sound3.mp3')
+    full_path = os.path.join(APP_FOLDER, "sound3.mp3")
+    mixer.music.load(full_path)
     pygame.mixer.music.play(-1)
 
 def setCountdown():
-    pygame.mixer.music.load('sounds/countdown.mp3')
+    full_path = os.path.join(APP_FOLDER, "countdown.mp3")
+    mixer.music.load(full_path)
     pygame.mixer.music.play()
 
 def stopMusic(): #stops the music
