ab0d5c93ee876ee68e1d77ae17f47da7ddec8814
Jesper Pedersen <54931767+Jesp9025@users.noreply.github.com>
2019-12-13 11:04:06 +0100

    Delete WhoIs.py

--- Old/WhoIs.py
+++
@@ -1,6 +0,0 @@
-#https://github.com/richardpenman/pywhois
-#https://www.pythonforbeginners.com/dns/using-pywhois
-import whois
-w = whois.whois("google.com")
-print(w)
-#It may say there is an error and whois is not callable.. Ignore it.
